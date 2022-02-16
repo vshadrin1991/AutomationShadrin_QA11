5e523de39e84d1a85972c97e42b5be73ea7b9b3d
Vitali Shadrin <vitali.shadrin.consultant@nielsen.com>
2022-02-16 19:27:12 +0300

    Update BasePage.java

--- src/main/java/PageObject/BasePage.java
+++ src/main/java/PageObject/BasePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
-    protected WebDriver driver = null;
+    protected WebDriver driver;
 
     protected BasePage(WebDriver driver) {
         this.driver = driver;
@@ -15,19 +15,9 @@ public abstract class BasePage {
         return this;
     }
 
-    protected BasePage open() {
-        driver.get("");
-        return this;
-    }
-
     protected BasePage enter(By element, CharSequence... data) {
         driver.findElement(element).clear();
         driver.findElement(element).sendKeys(data);
-        return this;
-    }
-
-    protected BasePage click(By element, Boolean test) {
-        driver.findElement(element).click();
         return this;
     }
 
